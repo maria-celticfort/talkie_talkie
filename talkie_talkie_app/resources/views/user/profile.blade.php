e3bce5829c9cde8de8dda0f441a5ee2d5f73fe68
maria-celticfortclear <mariacastrodarriba12@gmail.com>
2022-06-02 23:44:24 +0200

    [ENG][ESP]  UI cretaed/Creación de interfaz de usuario
    [ENG]
    -->:lipstick: The UI was created using Bootstrap
    -->:bug: Minor bugs were fixed
    -->:goal_net: Minor improvements were done
    
    [ESP]
    -->:lipstick: Se crea la interfaz de usuario usando Bootstrap
    -->:bug: Se arreglan errores menores
    -->:goal_net: Se implementan pequeñas mejoras

--- talkie_talkie_app/resources/views/user/profile.blade.php
+++ talkie_talkie_app/resources/views/user/profile.blade.php
@@ -1,25 +1,60 @@
 @extends('theme.base')
 
 @section('content')
-    <h1>Perfil</h1>
+    <div class="mt-2-custom container px-5">
+        @if (Session::has('resolution'))
+        <div class="alert alert-warning my-5" role="alert">
+            {{Session:: get('resolution')}}
+        </div>
+        @endif
 
-    @if (Session::has('resolution'))
-    <div class="alert alert-info my-5">
-        {{Session:: get('resolution')}}
+        <h1>Tu perfil</h1>
+        
+        <form action="#" method="#">
+                @csrf
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nombre</label>
+                    <input type="text" name="name" readonly class="form-control" placeholder="Tu nombre" value="{{@$user->name}}">
+                </div>
+
+                <div class="mb-3">
+                    <label for="surname_1" class="form-label">Primer apellido</label>
+                    <input type="text" name="surname_1" readonly class="form-control" placeholder="Primer apelldio" value="{{@$user->surname_1 }}" >
+                </div>
+
+                <div class="mb-3">
+                    <label for="surname_2" class="form-label">Segundo apellido</label>
+                    <input type="text" name="surname_2" readonly class="form-control" placeholder="Segundo apelldio" value="{{@$user->surname_2}}">
+                </div>
+
+                <div class="mb-3">    
+                    <label for="email" class="form-label">Mail</label>
+                    <input type="email" name="email" readonly class="form-control" placeholder="Email" value="{{@$user->email}}">
+                </div>
+
+                <div class="mb-3">
+                    <label for="nickname" class="form-label">Nickname</label>
+                    <input type="text" name="nickname" readonly class="form-control" placeholder="Nickname" value="{{@$user->nickname}}">
+                </div>
+
+                <div class="mb-3">
+                    <label for="date_of_birth" class="form-label">Fecha de nacimiento</label>
+                    <input type="date" name="date_of_birth" readonly class="form-control" value="{{@$user->date_of_birth}}">
+                </div>
+
+                <div class="mb-3">
+                    <label for="pronouns" class="form-label">Pronombres</label>
+                    <input type="text" name="pronouns" readonly class="form-control" value="{{@$user->pronouns}}">
+                </div>
+        </form>
+
+        <div class="d-flex justify-content-center">
+            <a class="btn btn-lg btn-warning rounded-pill" href="{{route('user.edit', $user)}}" role="button">Editar perfil</a>
+        </div>
+
+        <div class="return-btn-2">
+            <a class="btn btn-warning rounded-pill" href="{{route('index')}}" role="button"> ← Volver a Inicio</a>
+        </div>
+
     </div>
-    @endif
-
-    <div class="user_data">
-        Nombre: {{$user->name}} <br>
-        Primer apellido: {{$user->surname_1}} <br>
-        Segundo apellido: {{$user->surname_2}} <br>
-        Nickname: {{$user->nickname}} <br>
-        Email: {{$user->email}} <br>
-        Pronombres: {{$user->pronouns}} <br>
-        Fecha de nacimiento: {{$user->date_of_birth}} <br>
-    </div>
-
-    <a class="btn btn-primary" href="{{route('user.edit', $user)}}" role="button">Editar perfil</a>
-    <a class="btn btn-primary" href="{{route('user.logout')}}" role="button">Logout</a>
-    <a class="btn btn-primary" href="{{route('index')}}" role="button">Volver a Inicio</a>
 @endsection
